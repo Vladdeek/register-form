49d508de5d6de2c5b9efe65cd96ed3d2936352eb
Vladdeek <126556608+Vladdeek@users.noreply.github.com>
2024-11-11 09:56:35 +0200

    fix slepaya zone

--- js/modal.anim.js
+++ js/modal.anim.js
@@ -1,17 +1,21 @@
 let modal = document.querySelector('.auth-modal')
 let blurScreen = document.querySelector('.blur-screen')
+let hidebtn = document.querySelector('.hide-modal')
 
 modal.style.visibility = 'hidden'
 blurScreen.style.visibility = 'hidden'
+hidebtn.style.visibility = 'hidden'
 
 function showModal() {
 	console.log('кнопка нажата')
 	modal.style.visibility = 'visible'
 	blurScreen.style.visibility = 'visible'
+	hidebtn.style.visibility = 'visible'
 	modal.style.transform = 'translateY(25%)'
 }
 function hideModal() {
 	modal.style.visibility = 'hidden'
 	blurScreen.style.visibility = 'hidden'
+	hidebtn.style.visibility = 'hidden'
 	modal.style.transform = 'translateY(-125%)'
 }
